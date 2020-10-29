4d6ea8909509469e0f89a22da266862af40acc84
viktorholk <viktorholk@gmail.com>
2020-10-29 12:50:41 +0100

    Server socket

--- src/socket/Chat/server.cpp
+++ src/socket/Chat/server.cpp
@@ -1,59 +1,74 @@
-// Server side C/C++ program to demonstrate Socket programming 
-#include <unistd.h> 
-#include <stdio.h> 
-#include <sys/socket.h> 
-#include <stdlib.h> 
-#include <netinet/in.h> 
-#include <string.h> 
-#define PORT 8080 
+#define _WIN32_WINNT 0x501
+#include <iostream>
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iphlpapi.h>
+#include <stdio.h>
+#pragma comment(lib, "Ws2_32.lib")
+
+#define DEFAULT_PORT "27015" 
+
+
 int main(int argc, char const *argv[]) 
 { 
-    int server_fd, new_socket, valread; 
-    struct sockaddr_in address; 
-    int opt = 1; 
-    int addrlen = sizeof(address); 
-    char buffer[1024] = {0}; 
-    char *hello = "Hello from server"; 
-       
-    // Creating socket file descriptor 
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
-    { 
-        perror("socket failed"); 
-        exit(EXIT_FAILURE); 
-    } 
-       
-    // Forcefully attaching socket to the port 8080 
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, 
-                                                  &opt, sizeof(opt))) 
-    { 
-        perror("setsockopt"); 
-        exit(EXIT_FAILURE); 
-    } 
-    address.sin_family = AF_INET; 
-    address.sin_addr.s_addr = INADDR_ANY; 
-    address.sin_port = htons( PORT ); 
-       
-    // Forcefully attaching socket to the port 8080 
-    if (bind(server_fd, (struct sockaddr *)&address,  
-                                 sizeof(address))<0) 
-    { 
-        perror("bind failed"); 
-        exit(EXIT_FAILURE); 
-    } 
-    if (listen(server_fd, 3) < 0) 
-    { 
-        perror("listen"); 
-        exit(EXIT_FAILURE); 
-    } 
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,  
-                       (socklen_t*)&addrlen))<0) 
-    { 
-        perror("accept"); 
-        exit(EXIT_FAILURE); 
-    } 
-    valread = read( new_socket , buffer, 1024); 
-    printf("%s\n",buffer ); 
-    send(new_socket , hello , strlen(hello) , 0 ); 
-    printf("Hello message sent\n"); 
-    return 0; 
+    WSADATA wsaData;
+    // Initialize Winsock
+    if (WSAStartup(MAKEWORD(2,2), &wsaData) != 0){
+        printf("WSAStartup failed: %d", WSAGetLastError());
+        return 1;
+    }
+
+    struct addrinfo *result = NULL, *ptr = NULL, hints;
+
+    ZeroMemory(&hints, sizeof(hints));
+    hints.ai_family     = AF_INET;
+    hints.ai_socktype   = SOCK_STREAM;
+    hints.ai_protocol   = IPPROTO_TCP;
+    hints.ai_flags      = AI_PASSIVE;
+
+    // Resolve the local address and DEFAULT_PORT to be used by the server
+    if (getaddrinfo(NULL, DEFAULT_PORT, &hints, &result)){
+        printf("getaddrinfo failed: %d\n", GetLastError());
+        WSACleanup();
+        return 1;
+    }
+    SOCKET listenSocket = INVALID_SOCKET;
+
+    listenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+    if ( listenSocket == INVALID_SOCKET){
+        printf("Error at socket(): %ld\n", WSAGetLastError());
+        freeaddrinfo(result);
+        WSACleanup();
+        return 1;
+    }
+
+    if ( bind(listenSocket, result->ai_addr, (int)result->ai_addrlen)){
+        printf("bind failed with error: %d\n", WSAGetLastError());
+        freeaddrinfo(result);
+        closesocket(listenSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    if ( listen( listenSocket, SOMAXCONN ) == SOCKET_ERROR ) {
+        printf("Listen failed with error: %ld\n", WSAGetLastError());
+        closesocket(listenSocket);
+        WSACleanup();
+        return 1;
+    }
+    printf("Ready\n");
+    
+    SOCKET ClientSocket = INVALID_SOCKET;
+
+    // Accept a client socket
+    ClientSocket = accept(listenSocket, NULL, NULL);
+    if (ClientSocket == INVALID_SOCKET){
+        printf("accept failed: %d\n", WSAGetLastError());
+        closesocket(listenSocket);
+        WSACleanup();
+        return 1;
+    }
+
+
 } 
